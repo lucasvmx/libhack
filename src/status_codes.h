8a1ba336e0b9784830b6cbf596b9590871ab8a1d
Lucas Vieira <lucas.engen.cc@gmail.com>
2022-11-20 18:07:01 -0300

    fix: fixes building for windows
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- src/status_codes.h
+++ src/status_codes.h
@@ -2,16 +2,17 @@
 #define LIBHACK_STATUS_CODES_H
 
 #include "platform.h"
-#include <sys/types.h>
 
 #ifdef __windows__
 #include <stdint.h>
+#elif __linux__
+#include <sys/types.h>
 #endif
 
 #define LIBHACK_OK 0
 
 #ifdef __windows__
-typedef __int32_t int32_t
+typedef int32_t __int32_t;
 #endif
 
 __int32_t libhack_get_last_error();
