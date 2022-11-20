0234c92bcb68979a0696ecbfd3dd2ce8d46ff161
Lucas Vieira <lucas.engen.cc@gmail.com>
2022-11-20 16:56:03 -0300

    chore: adds functions to handle error codes
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- src/status_codes.h
+++ src/status_codes.h
@@ -1,6 +1,11 @@
 #ifndef LIBHACK_STATUS_CODES_H
 #define LIBHACK_STATUS_CODES_H
 
+#include <sys/types.h>
+
 #define LIBHACK_OK 0
+
+__int32_t libhack_get_last_error();
+void libhack_set_last_error(__int32_t err);
 
 #endif
